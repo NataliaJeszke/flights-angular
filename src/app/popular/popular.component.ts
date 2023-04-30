9b3e42fabfe98429c28b15d8059698669435ffcb
NataliaJeszke <nataliajeszke8@gmail.com>
2023-04-30 19:52:27 +0200

    static list for popular flights

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -1,39 +1,38 @@
 import { Component } from '@angular/core';
-import { FlightData, FlightDataInterface } from '../models/FlightDataInterface';
+import { FlightData } from '../models/FlightDataInterface';
 import { HttpFlightsService } from '../services/http-flights.service';
 
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
-  styleUrls: ['./popular.component.css']
+  styleUrls: ['./popular.component.css'],
 })
 export class PopularComponent {
+  constructor(private http: HttpFlightsService) {}
 
-  constructor (private http: HttpFlightsService) {}
+  flights: FlightData[] = [];
 
   ngOnInit(): void {
-    this.get()};
+    this.get();
+  }
 
-  flights:FlightData[] = [];
-
-  get(){
-    this.http.getFlights().subscribe(response => {
+  get() {
+    this.http.getFlights().subscribe((response) => {
       this.flights = response;
       console.log(this.flights);
-    }); 
-}
+    });
+  }
 
-get randomFlights() {
-  const sortedFlights = this.flights.sort(() => 0.5 - Math.random());
-  return sortedFlights.slice(0, 5);
-}
+  get popularFlights() {
+    const popularFlights = this.flights;
+    return popularFlights.slice(0, 5);
+  }
 
-formatArrivalDate(flight: FlightData): string {
-  return new Date(flight.arrival * 1000).toLocaleString();
-}
+  formatArrivalDate(flight: FlightData): string {
+    return new Date(flight.arrival * 1000).toLocaleString();
+  }
 
   choseFlight() {
     console.log('Chose flight');
   }
-
 }
